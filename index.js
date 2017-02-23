4aa81347a5fbe3d59dfef850597aab6a607cfcdc
Josh Swan <josh@sportifik.com>
2017-02-23 00:22:11 -0800

    Implement returning logic within sync methods instead of waiting for model events

--- index.js
+++ index.js
@@ -9,47 +9,69 @@
 const Promise = require('bluebird');
 const assign = require('lodash/assign');
 const extend = require('lodash/extend');
+const filter = require('lodash/filter');
+
+function handleResponse(resp, syncing, options) {
+  if (options.returning) {
+    // Check for require option and empty result, return 0 to trigger NoRowsUpdatedError
+    if (options.method === 'update' && options.require !== false && !resp.length) return 0;
+
+    // Save previous attributes
+    // eslint-disable-next-line no-underscore-dangle
+    options.previousAttributes = syncing._previousAttributes = syncing.attributes;
+
+    if (typeof resp[0] === 'object') {
+      // Assign returned values to attributes
+      syncing.attributes = assign({}, syncing.parse(syncing.attributes), syncing.parse(resp[0]));
+
+      // Update model's ID
+      syncing.id = syncing.attributes[syncing.idAttribute] || syncing.id;
+    }
+  }
+
+  // Return raw database result
+  return resp;
+}
 
 module.exports = (bookshelf) => {
-  const BaseModel = bookshelf.Model;
+  const ModelBase = bookshelf.Model;
 
-  bookshelf.Model = BaseModel.extend({
-    initialize(...args) {
-      this.on('saving', (model, attrs, options) => {
-        if (options.returning) {
-          options.query.returning(options.returning);
-        }
-      });
-
-      this.on('saved', (model, resp, options) => {
-        if (options.returning) {
-          // Throw NoRowsUpdatedError for updates with require option and nothing returned
-          if (options.method === 'update' && options.require !== false && !resp.length) {
-            throw new this.constructor.NoRowsUpdatedError('No Rows Updated');
-          }
-
-          if (typeof resp[0] === 'object') {
-            // Assign returned values to attributes
-            assign(this.attributes, this.parse(resp[0]));
-
-            // Update model's ID
-            this.id = this.attributes.id || null;
-          }
-        }
-      });
-
-      // Call parent initialize method
-      BaseModel.prototype.initialize.apply(this, args);
-    },
-    // Override sync insert method to respect options.returning
+  bookshelf.Model = ModelBase.extend({
+    // Override sync insert & update methods to implement options.returning
     sync(...args) {
-      const sync = BaseModel.prototype.sync.apply(this, args);
+      const sync = ModelBase.prototype.sync.apply(this, args);
 
       sync.insert = Promise.method(function syncInsert() {
-        const syncing = this.syncing;
+        const {
+          options,
+          query,
+          syncing,
+        } = this;
+
         const attributes = syncing.format(extend(Object.create(null), syncing.attributes));
 
-        return this.query.insert(attributes, this.options.returning || syncing.idAttribute);
+        return query.insert(attributes, options.returning || syncing.idAttribute)
+          .then(resp => handleResponse(resp, syncing, options));
+      });
+
+      sync.update = Promise.method(function syncUpdate(attrs) {
+        const {
+          options,
+          query,
+          syncing,
+        } = this;
+
+        if (syncing.id != null) query.where(syncing.idAttribute, syncing.id);
+
+        // eslint-disable-next-line no-underscore-dangle
+        if (filter(query._statements, { grouping: 'where' }).length === 0) {
+          throw new Error('A model cannot be updated without a "where" clause or an idAttribute.');
+        }
+
+        const attributes = syncing.format(extend(Object.create(null), attrs));
+
+        return query.update(attributes, options.returning || null)
+          .then(resp => handleResponse(resp, syncing, options));
       });
 
       return sync;
